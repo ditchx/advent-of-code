89e4474d2894529ae2278efa5b3eba7fd223b986
Rolan Batungbakal <understasis@gmail.com>
2023-12-02 23:42:45 +0800

    AOC 2023 Day 2 Puzzle 2

--- 2023/go/day02/main.go
+++ 2023/go/day02/main.go
@@ -21,13 +21,44 @@ func main() {
 	scanner := bufio.NewScanner(fp)
 	scanner.Split(bufio.ScanLines)
 
-	var total uint
+	var puzzle1 uint
+	var puzzle2 uint
+
 	for scanner.Scan() {
-		total += validateGame([]byte(scanner.Text()))
+		line := []byte(scanner.Text())
+		puzzle1 += validateGame(line)
+		puzzle2 += getPower(line)
 	}
 
-	fmt.Println(total)
+	fmt.Printf("Puzzle 1: %d\n", puzzle1)
+	fmt.Printf("Puzzle 2: %d\n", puzzle2)
 
+}
+
+func getPower(line []byte) uint {
+	var minCubes = map[string]uint{
+		"red":   0,
+		"green": 0,
+		"blue":  0,
+	}
+
+	game := strings.Split(string(line), ":")
+	sets := strings.Split(game[1], ";")
+
+	for _, s := range sets {
+		cubes := strings.Split(s, ",")
+		for _, c := range cubes {
+			var count uint
+			var color string
+			fmt.Sscanf(c, "%d %s", &count, &color)
+
+			if count >= minCubes[color] {
+				minCubes[color] = count
+			}
+		}
+	}
+
+	return minCubes["red"] * minCubes["green"] * minCubes["blue"]
 }
 
 func validateGame(line []byte) uint {
