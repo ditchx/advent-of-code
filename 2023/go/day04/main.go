12a8895beb3e2cdf52eeac728b651d2472dd56f7
Rolan Batungbakal <understasis@gmail.com>
2023-12-04 20:55:22 +0800

    AOC 2023 Day 4 Puzzle 2

--- 2023/go/day04/main.go
+++ 2023/go/day04/main.go
@@ -16,12 +16,25 @@ func main() {
 	scanner.Split(bufio.ScanLines)
 
 	var total uint64
+	cards := make(map[string]uint)
+	var cardNum uint
+
 	for scanner.Scan() {
-		total += processCards(scanner.Text())
+		line := scanner.Text()
+		total += processCards(line)
+
+		countCards(cardNum, line, cards)
+		cardNum++
 	}
 
 	fmt.Println(total)
 
+	var cardTotal uint
+	for _, v := range cards {
+		cardTotal += v
+	}
+
+	fmt.Println(cardTotal)
 }
 
 func processCards(line string) uint64 {
@@ -48,4 +61,42 @@ func processCards(line string) uint64 {
 	}
 
 	return points
+}
+
+func countCards(cardNum uint, line string, cards map[string]uint) {
+	parts := strings.Split(line, ":")
+	sets := strings.Split(parts[1], "|")
+
+	var winning = make(map[string]struct{})
+	for _, v := range strings.Fields(sets[0]) {
+		winning[v] = struct{}{}
+	}
+
+	var index = fmt.Sprint(cardNum)
+	val, ok := cards[index]
+
+	if !ok {
+		val = 1
+		cards[index] = val
+	}
+
+	var totalWins = 0
+	for _, v := range strings.Fields(sets[1]) {
+		if _, ok := winning[v]; ok {
+			totalWins++
+
+			nextCard := fmt.Sprint((cardNum + uint(totalWins)))
+
+			nextVal, nextOk := cards[nextCard]
+
+			if !nextOk {
+				nextVal = 1
+				cards[nextCard] = nextVal
+			}
+
+			cards[nextCard] += val
+
+		}
+	}
+
 }
