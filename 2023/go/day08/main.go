d09f2347a881f8ad449ef82b9f7acb2855b530c1
Rolan Batungbakal <understasis@gmail.com>
2023-12-08 18:06:09 +0800

    AOC 2023 Day 8 Puzzle 2

--- 2023/go/day08/main.go
+++ 2023/go/day08/main.go
@@ -33,11 +33,17 @@ func main() {
 
 	nodeMap := make(map[string]*Node)
 
+	startingNodes := make([]string, 0)
+
 	for _, line := range lines {
 		data := strings.Fields(line)
 
 		if len(data) == 0 {
 			continue
+		}
+
+		if 'A' == data[0][2] {
+			startingNodes = append(startingNodes, data[0])
 		}
 
 		if _, ok := nodeMap[data[0]]; !ok {
@@ -79,4 +85,52 @@ func main() {
 	}
 
 	fmt.Println(stepCount)
+
+	endPoints := make([]uint64, 0)
+
+	for _, current = range startingNodes {
+		found = false
+		stepCount = 0
+		for !found {
+			for _, j := range steps {
+				if 'L' == j {
+					current = nodeMap[current].Left.Name
+				} else {
+					current = nodeMap[current].Right.Name
+				}
+
+				stepCount += 1
+				if 'Z' == current[2] {
+					found = true
+					break
+				}
+			}
+		}
+		endPoints = append(endPoints, stepCount)
+	}
+
+	stepCount = LCM(endPoints[0], endPoints[1], endPoints[2:]...)
+	fmt.Println(stepCount)
+}
+
+// The following functions were shamelessly copied
+// and modifed from https://siongui.github.io/2017/06/03/go-find-lcm-by-gcd/
+func GCD(a, b uint64) uint64 {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// find Least Common Multiple (LCM) via GCD
+func LCM(a, b uint64, integers ...uint64) uint64 {
+	result := a * b / GCD(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = LCM(result, integers[i])
+	}
+
+	return result
 }
