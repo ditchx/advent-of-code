e7a4c10037e2c745e72dd150fdf2410297d45a59
Rolan Batungbakal <understasis@gmail.com>
2023-12-26 17:53:34 +0800

    AOC 2023 Day 15 Puzzle 2

--- 2023/go/day15/main.go
+++ 2023/go/day15/main.go
@@ -3,8 +3,21 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
+
+type Lens struct {
+	Label       string
+	FocalLength uint8
+}
+
+func newLens(label string, focalLength uint8) Lens {
+	return Lens{
+		Label:       label,
+		FocalLength: focalLength,
+	}
+}
 
 func main() {
 	file, _ := os.ReadFile(os.Args[1])
@@ -17,6 +30,8 @@ func main() {
 	}
 
 	fmt.Println(total)
+
+	fmt.Println(hashmap(contents))
 }
 
 func hash(data string) uint64 {
@@ -29,4 +44,54 @@ func hash(data string) uint64 {
 	}
 
 	return current
+}
+
+func hashmap(data []string) uint64 {
+	var totalFocusingPower uint64
+
+	boxes := make([][]Lens, 256)
+
+	for i := range data {
+		if strings.Contains(data[i], "=") {
+			parts := strings.Split(data[i], "=")
+
+			boxNumber := hash(parts[0])
+			lens, _ := strconv.ParseUint(parts[1], 10, 32)
+
+			found := false
+			for x := range boxes[boxNumber] {
+				if boxes[boxNumber][x].Label == parts[0] {
+					boxes[boxNumber][x].FocalLength = uint8(lens)
+					found = true
+				}
+			}
+
+			if false == found {
+				boxes[boxNumber] = append(boxes[boxNumber], newLens(parts[0], uint8(lens)))
+			}
+
+			continue
+		}
+
+		label := strings.TrimRight(data[i], "-")
+		boxNumber := hash(label)
+
+		for x := range boxes[boxNumber] {
+			if boxes[boxNumber][x].Label == label {
+				copy(boxes[boxNumber][x:], boxes[boxNumber][x+1:])
+				boxes[boxNumber][len(boxes[boxNumber])-1] = Lens{}
+				boxes[boxNumber] = boxes[boxNumber][:len(boxes[boxNumber])-1]
+				break
+			}
+		}
+
+	}
+
+	for boxNumber := range boxes {
+		for slotNumber := range boxes[boxNumber] {
+			totalFocusingPower += uint64(boxNumber+1) * uint64(slotNumber+1) * uint64(boxes[boxNumber][slotNumber].FocalLength)
+		}
+	}
+
+	return totalFocusingPower
 }
