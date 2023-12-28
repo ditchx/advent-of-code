b17d4c9d36a90b3770c4acb2689be34c1e92e3c1
Rolan Batungbakal <understasis@gmail.com>
2023-12-28 15:58:30 +0800

    AOC 2023 Day 16 Puzzle 1

---
+++ 2023/go/day16/main.go
@@ -0,0 +1,186 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+const up string = "UP"
+const down string = "DOWN"
+const left string = "LEFT"
+const right string = "RIGHT"
+
+type Ray struct {
+	X         int
+	Y         int
+	Direction string
+}
+
+var errOutOfBounds error = fmt.Errorf("Ray out of bounds")
+
+func (r Ray) Next() Ray {
+	nextRay := newRay(r.X, r.Y, r.Direction)
+
+	switch r.Direction {
+	case up:
+		nextRay.X--
+	case down:
+		nextRay.X++
+	case left:
+		nextRay.Y--
+	case right:
+		nextRay.Y++
+	}
+
+	return nextRay
+}
+
+func newRay(x int, y int, direction string) Ray {
+	return Ray{
+		X:         x,
+		Y:         y,
+		Direction: direction,
+	}
+}
+
+func main() {
+	file, _ := os.ReadFile(os.Args[1])
+	grid := strings.Split(strings.TrimSpace(string(file)), "\n")
+
+	tiles := make(map[string][]Ray)
+
+	initial := newRay(0, 0, right)
+	beam(grid, tiles, initial)
+
+	fmt.Println(len(tiles))
+}
+
+func beam(grid []string, tiles map[string][]Ray, r Ray) {
+
+	if !checkBoundary(r, grid) {
+		return
+	}
+
+	k := makeKey(r.X, r.Y)
+	// If tile already contains
+	// a ray with same direction,
+	// this means we already passed this
+	// and should exit to stop cycling/looping
+	if rayList, ok := tiles[k]; ok {
+		for i := range rayList {
+			if rayList[i].Direction == r.Direction {
+				return
+			}
+		}
+	}
+	tiles[k] = append(tiles[k], r)
+
+	currentTile := grid[r.X][r.Y]
+
+	if '.' == currentTile {
+		nextRay := r.Next()
+		beam(grid, tiles, nextRay)
+		return
+	}
+
+	if '|' == currentTile {
+		if left == r.Direction || right == r.Direction {
+
+			upRay := newRay(r.X-1, r.Y, up)
+			downRay := newRay(r.X+1, r.Y, down)
+
+			beam(grid, tiles, upRay)
+			beam(grid, tiles, downRay)
+		} else {
+			nextRay := r.Next()
+			beam(grid, tiles, nextRay)
+		}
+
+		return
+	}
+
+	if '-' == currentTile {
+		if up == r.Direction || down == r.Direction {
+
+			leftRay := newRay(r.X, r.Y-1, left)
+			rightRay := newRay(r.X, r.Y+1, right)
+
+			beam(grid, tiles, leftRay)
+			beam(grid, tiles, rightRay)
+
+		} else {
+			nextRay := r.Next()
+			beam(grid, tiles, nextRay)
+
+		}
+		return
+	}
+
+	if '/' == currentTile {
+		nextRay := newRay(r.X, r.Y, r.Direction)
+		switch nextRay.Direction {
+		case up:
+			nextRay.Direction = right
+			nextRay.Y++
+		case down:
+			nextRay.Direction = left
+			nextRay.Y--
+		case left:
+			nextRay.Direction = down
+			nextRay.X++
+		case right:
+			nextRay.Direction = up
+			nextRay.X--
+		}
+
+		beam(grid, tiles, nextRay)
+		return
+	}
+
+	if '\\' == currentTile {
+		nextRay := newRay(r.X, r.Y, r.Direction)
+		switch nextRay.Direction {
+		case up:
+			nextRay.Direction = left
+			nextRay.Y--
+		case down:
+			nextRay.Direction = right
+			nextRay.Y++
+		case left:
+			nextRay.Direction = up
+			nextRay.X--
+		case right:
+			nextRay.Direction = down
+			nextRay.X++
+		}
+
+		beam(grid, tiles, nextRay)
+		return
+	}
+
+}
+
+func makeKey(x, y int) string {
+	return fmt.Sprintf("%d-%d", x, y)
+}
+
+func checkBoundary(r Ray, grid []string) bool {
+	if r.X < 0 {
+		return false
+	}
+
+	if r.X > len(grid)-1 {
+		return false
+	}
+
+	if r.Y < 0 {
+		return false
+	}
+
+	if r.Y > len(grid[0])-1 {
+		return false
+	}
+
+	return true
+}
